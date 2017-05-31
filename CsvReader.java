560c9f79001dc36343bd25b07774684115db850c
Juliana Barbosa <jubsbarbosa0@gmail.com>
2017-05-30 21:15:05 -0300

    lendo arquivos

--- CsvReader.java
+++ CsvReader.java
@@ -9,15 +9,15 @@ public class CsvReader{
 	public CsvReader(){}
 
 	public static void main(String [] args) throws Exception{
-		BufferedReader fileLine = new BufferedReader(new File("/home/juliana/Documents/UFRN/2017.1/LP II/InsiderThreat/device.csv"));
+		BufferedReader fileLine = new BufferedReader(new FileReader("/home/juliana/Documents/UFRN/2017.1/LP II/InsiderThreat/device.csv"));
 		ArrayList<String> arrayLine = new ArrayList<String>();
-		String line;
+		String line = null;
 
 		while((line = fileLine.readLine()) !=null){
-			arrayLine.add(line);
+			arrayLine.add(fileLine.readLine());
 		}
-		for(int i = 0; i < fileLine.size(); i++){
-			System.out.println(fileLine.get(i));
-		}
+		for(int i = 0; i < arrayLine.size(); i++){
+			System.out.println(arrayLine.get(i));
+		} 
 	}
 }
